e668414a06d2376b4695a4fe25dffa72e2af4bec
Riddhi Jain <riddhijain1003@gmail.com>
2025-12-25 09:05:31 +0530

    add firebase

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-console.log("Platypus JS loaded");
+// Firebase config (PASTE YOUR CONFIG HERE)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// LOGIN BUTTON
-const loginBtn = document.querySelector("button");
+const firebaseConfig = {
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  appId: "PASTE_HERE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// LOGIN
+const loginBtn = document.getElementById("loginBtn");
 
 if (loginBtn) {
   loginBtn.addEventListener("click", () => {
-    alert("Login button clicked!");
-    window.location.href = "dashboard.html";
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const error = document.getElementById("error");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.href = "dashboard.html";
+      })
+      .catch((err) => {
+        error.innerText = err.message;
+      });
   });
 }
